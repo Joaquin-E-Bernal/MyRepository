8e18bb2e93d2c0b856bf6bc98fd32085acf2c078
Joaquin-E-Bernal <137236728+Joaquin-E-Bernal@users.noreply.github.com>
2023-06-22 15:15:36 -0300

    Delete Mis practicas directory

--- Mis practicas/ejer_parcial/puts.c
+++
@@ -1,3 +0,0 @@
-/*	puts("");	permite imprimir pantalla y copiar su texto.
-
-variable tipo static: se mantiene inicializada durante todo el programa cuando se ingresa 1 vez a la funcion q la contiene.
